9fa7091e39ced284a76b2656c4555e761686ca13
jjhubbard <jordanjhubbard@gmail.com>
2025-04-13 22:58:05 -0700

    Fix set 16-bit registers fn to treat input as little endian

--- src/cpu/registers.zig
+++ src/cpu/registers.zig
@@ -1,5 +1,4 @@
 const std = @import("std");
-const assert = std.debug.assert;
 const testing = std.testing;
 
 const ZERO_FLAG_BYTE_POSITION: u3 = 7;
@@ -22,24 +21,24 @@ pub const Registers = struct {
     }
 
     pub fn set_af(self: *Registers, value: u16) void {
-        self.a = @truncate((value & 0xFF00) >> 8);
-        self.f = flagsFromU8(@truncate(value & 0x00FF));
+        self.a = @truncate(value & 0x00FF);
+        self.f = flagsFromU8(@truncate((value & 0xFF00) >> 8));
     }
     pub fn get_bc(self: Registers) u16 {
         return @as(u16, self.b) << 8 | @as(u16, self.c);
     }
 
     pub fn set_bc(self: *Registers, value: u16) void {
-        self.b = @truncate((value & 0xFF00) >> 8);
-        self.c = @truncate(value & 0x00FF);
+        self.b = @truncate(value & 0x00FF);
+        self.c = @truncate((value & 0xFF00) >> 8);
     }
     pub fn get_de(self: Registers) u16 {
         return @as(u16, self.d) << 8 | @as(u16, self.e);
     }
 
     pub fn set_de(self: *Registers, value: u16) void {
-        self.d = @truncate((value & 0xFF00) >> 8);
-        self.e = @truncate(value & 0x00FF);
+        self.d = @truncate(value & 0x00FF);
+        self.e = @truncate((value & 0xFF00) >> 8);
     }
 
     pub fn get_hl(self: Registers) u16 {
@@ -47,8 +46,8 @@ pub const Registers = struct {
     }
 
     pub fn set_hl(self: *Registers, value: u16) void {
-        self.h = @truncate((value & 0xFF00) >> 8);
-        self.l = @truncate(value & 0x00FF);
+        self.h = @truncate(value & 0x00FF);
+        self.l = @truncate((value & 0xFF00) >> 8);
     }
 };
 
@@ -103,30 +102,30 @@ test "Registers.get_hl" {
 }
 
 test "Registers.set_af" {
-    try testing.expect(test_register.get_af() != 0b1100110011010000);
-    test_register.set_af(0b1100110011010000);
-    try testing.expect(test_register.get_af() == 0b1100110011010000);
+    try testing.expect(test_register.get_af() != 0b1100000011010000);
+    test_register.set_af(0b1100000011010000);
+    try testing.expect(test_register.get_af() == 0b1101000011000000);
 }
 test "Registers.set_af_non_zero_lower_4" {
     try testing.expect(test_register.get_af() != 0b1100110011000000);
     test_register.set_af(0b1100110011000011);
-    try testing.expect(test_register.get_af() == 0b1100110011000000);
+    try testing.expect(test_register.get_af() == 0b1100001111000000);
 }
 
 test "Registers.set_bc" {
-    try testing.expect(test_register.get_bc() != 0b1100110011000011);
-    test_register.set_bc(0b1100110011000011);
+    try testing.expect(test_register.get_bc() != 0b1100001111001100);
+    test_register.set_bc(0b1100001111001100);
     try testing.expect(test_register.get_bc() == 0b1100110011000011);
 }
 
 test "Registers.set_de" {
-    try testing.expect(test_register.get_de() != 0b1100110011000011);
-    test_register.set_de(0b1100110011000011);
+    try testing.expect(test_register.get_de() != 0b1100001111001100);
+    test_register.set_de(0b1100001111001100);
     try testing.expect(test_register.get_de() == 0b1100110011000011);
 }
 
 test "Registers.set_hl" {
-    try testing.expect(test_register.get_hl() != 0b1100110011000011);
-    test_register.set_hl(0b1100110011000011);
+    try testing.expect(test_register.get_hl() != 0b1100001111001100);
+    test_register.set_hl(0b1100001111001100);
     try testing.expect(test_register.get_hl() == 0b1100110011000011);
 }
